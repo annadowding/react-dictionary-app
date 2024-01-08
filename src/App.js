8efbb50cdb861a80800d9b64aa796159dcaa2798
Anna Dowding <144890388+annadowding@users.noreply.github.com>
2024-01-08 13:51:17 +0000

    Added react files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.png';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img 
+        src={logo} 
+        className="App-logo img-fluid" 
+        alt="logo" 
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
