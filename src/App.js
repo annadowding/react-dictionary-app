3297ff4b428b9101d1a5a2e094047f41b1eca73c
Anna Dowding <144890388+annadowding@users.noreply.github.com>
2024-01-10 16:05:49 +0000

    Updated links in footer

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <img
-          src={logo}
-          className="App-logo img-fluid"
-          alt="logo"
-          />
-          <Dictionary />
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+        <Dictionary />
       </header>
       <footer>
-        Coded by Anna Dowding
+        Coded by <a href="https://github.com/annadowding">Anna Dowding</a> and
+        is{" "}
+        <a href="https://github.com/annadowding/react-dictionary-app">
+          open-sourced
+        </a>{" "}
+        and hosted on{" "}
+        <a href="https://superlative-alpaca-174311.netlify.app/">Netlify</a>.
       </footer>
     </div>
   );
