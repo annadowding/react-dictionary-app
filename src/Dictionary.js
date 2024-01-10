8a9b8f220371d709582494a792c787c856440c34
Anna Dowding <144890388+annadowding@users.noreply.github.com>
2024-01-10 12:05:29 +0000

    Integrated default search on load

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,17 +4,21 @@ import axios from "axios";
 import Results from "./Results.js";
 
 export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+  let [keyword, setKeyword] = useState("sunset");
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded]= useState(false);
 
   function handleResponse(response) {
     setResults(response.data);
   }
 
-  function search(event) {
+  function handleSubmit (event) {
     event.preventDefault();
+    search();
     alert(`Searching for ${keyword} definition`);
+  }
 
+  function search() {
     //documentation: https://www.shecodes.io/learn/apis/dictionary
     let apiKey = "c695b4fc90b605eea29b70ecbaft3f9o";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
@@ -25,12 +29,22 @@ export default function Dictionary() {
     setKeyword(event.target.value);
   }
 
+  function load () {
+    setLoaded(true);
+    search();
+  }
+
+if (loaded) {
   return (
     <div className="Dictionary container">
-      <form onSubmit={search} className="searchbar">
+      <form onSubmit={handleSubmit} className="searchbar">
         <input type="search" autofocus onChange={handleKeywordChangeSet} />
       </form>
-      <Results results={results}/>
+      <Results results={results} />
     </div>
   );
+} else {
+    load();
+}
+  
 }
